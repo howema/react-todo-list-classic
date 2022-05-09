87d5c5da9131c12957c949ebbbc03c1677ce4990
Meg Howe <meg@heysparkbox.com>
2022-05-08 18:42:40 -0700

    feat: give each todo unique id

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import TodoList from './TodoList';
 
 function App() {
-  const [todos, setDoTos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setDoTos] = useState([{ id: 1, name: 'Todo 1', complete: false }])
  return (
    <>
    <TodoList todos={todos} />
